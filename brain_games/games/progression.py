679f53d7c1244945d5da48874a8273c69caae14a
Eugen980 <zaza980q@mail.ru>
2023-09-27 18:35:19 +0300

    eight step

---
+++ brain_games/games/progression.py
@@ -0,0 +1,17 @@
+from random import randint
+print('brain-progression')
+MANUAL = 'What number is missing in the progression?'
+
+
+def init_game():
+    sequence = []
+    step = randint(1, 5)
+    len_seq = randint(5, 10)
+    first_element = randint(1, 30)
+    random_element = randint(0, len_seq - 1)
+    for i in range(first_element, first_element + len_seq+1):
+        sequence.append(str(i*step))
+    answer = sequence[random_element]
+    sequence[random_element] = '..'
+    print(f'Question: ', *sequence)
+    return answer
