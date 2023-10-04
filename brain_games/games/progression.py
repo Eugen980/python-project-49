2296ef2d7049225da28900d52a007cf3fddfb18d
Eugen980 <144818317+Eugen980@users.noreply.github.com>
2023-10-04 18:51:07 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,10 +6,10 @@ MANUAL = 'What number is missing in the progression?'
 def init_game():
     sequence = []
     step = randint(1, 5)
-    len_seq = randint(5, 10)
+    len_seq = 11
     first_element = randint(1, 30)
     random_element = randint(0, len_seq - 1)
-    for i in range(first_element, first_element + len_seq+1):
+    for i in range(first_element, first_element + len_seq):
         sequence.append(str(i*step))
     answer = sequence[random_element]
     sequence[random_element] = '..'
