f8afade9adff77c7e7da34b242534dd89bff8b0b
Eugen980 <144818317+Eugen980@users.noreply.github.com>
2023-10-19 16:32:18 +0300

    Update engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,7 +1,7 @@
 import prompt
 
-
-def engine(game):  # Так и не понял про передачу конкретного значения вместо модуля
+# Так и не понял про передачу конкретного значения вместо модуля
+def engine(game):  
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
