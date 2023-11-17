b56bee73ff68087c273311bf5a751882def664c3
Eugen980 <zaza980q@mail.ru>
2023-11-17 17:49:28 +0300

    x

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,19 +1,14 @@
-from brain_games.random_number import random_number
+import random
 from brain_games.engine import run_game
 from brain_games.consts import NAME_GAME_EVEN, EVEN_INSTR
 
 
-def is_even(num):
-    result = 'yes' if num % 2 == 0 else 'no'
-    return result
-
-
-def get_desired_numer_and_answer():
-    num = random_number(b=100)
-    answer = is_even(num)
+def get_num_and_answer():
+    num = random.randint(1, 100)
+    answer = 'yes' if num % 2 == 0 else 'no'
     return num, answer
 
 
 def run_even_game():
     print(NAME_GAME_EVEN)
-    run_game(get_desired_numer_and_answer(), EVEN_INSTR)
+    run_game(get_num_and_answer(), EVEN_INSTR)
